4ead82870a91c1899c6b8cc22ef3e98c62d0fe15
AmandaCleto <amanda.clet@gmail.com>
2021-08-23 22:54:24 -0300

    get user route and test route

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,10 +4,17 @@ const router = Router();
 const todoController = require('../controllers/todo');
 const userController = require('../controllers/user');
 
+router.post('/user', userController.create);
+router.get('/user', userController.get);
+
+
 router.post('/todo', todoController.create);
 
 
-router.post('/user', userController.create);
-
+router.get('/teste', (req, res, next) => {
+    res.status(200).send({
+        mensagem: "funcionou 🚀"
+    });
+})
 
 module.exports = router;
